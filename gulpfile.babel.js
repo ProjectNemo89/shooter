ea2e10b48ca2b624a36f84d5e9a0182f7b49fa1a
Nemanja Miljkovic <nmiljkovic89@gmail.com>
2016-10-30 21:14:52 +0100

    From header to equipment section

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -5,27 +5,28 @@ import cssmin from 'gulp-clean-css';
 import postcssImport from 'postcss-import';
 import webpack from 'webpack';
 import del from 'del';
+import sync from 'browser-sync';
 
-const srcCSS = "./assets/style.css";
-const destCSS = "./build/css";
+const srcCSS = "./app/assets/style.css";
+const destCSS = "./app/build/css";
 const webpackConfig = require("./webpack.config.js");
-
-
+const syncOpts = {
+	server: {
+		baseDir: 'app'	
+	}	
+};
 
 gulp.task("css", () => {
 	return gulp.src(srcCSS)
 	.pipe(postcss([postcssImport, precss]))
-	.pipe(cssmin({compatibility: "ie8", debug: true}, (details) => {
-		console.log(details.name + ': ' + details.stats.originalSize + " bytes");
-        console.log(details.name + ': ' + details.stats.minifiedSize + " bytes");
-	}))
 	.on('error', (err) => {
 		if(err) {
 			console.log(err.toString());
 		}
 		this.emit('end');
 	})
-	.pipe(gulp.dest(destCSS));
+	.pipe(gulp.dest(destCSS))
+	.pipe(sync.stream());
 });
 
 gulp.task('js', () => {
@@ -39,15 +40,25 @@ gulp.task('js', () => {
 });
 
 gulp.task('clean', () => {
-	del(["./build/**", "!./build"]);
+	del(["./app/build/**", "!./app/build"]);
+});
+
+gulp.task('refresh', () => {
+	sync(syncOpts);
 });
 
 
-gulp.task("default", () => {
-	gulp.watch("./assets/style.css", ["css"]);
-	gulp.watch("./assets/js/app.js", ["js"]); 
+gulp.task("default", ["refresh"], () => {
+	gulp.watch(["./app/assets/css/modules/*.css", "./app/assets/*.css"], ["css"]);
+	gulp.watch("./app/assets/js/app.js", ["js"], () => sync.reload()); 
+	gulp.watch("./app/index.html", () => sync.reload());
 });
 
+
+	// .pipe(cssmin({compatibility: "ie8", debug: true}, (details) => {
+	// 	console.log(details.name + ': ' + details.stats.originalSize + " bytes");
+ //        console.log(details.name + ': ' + details.stats.minifiedSize + " bytes");
+	// }))
 
 
 
