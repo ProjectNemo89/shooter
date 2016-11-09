a1a88f137708aef02c42ffa96d11b1f45ead2539
Nemanja Miljkovic <nmiljkovic89@gmail.com>
2016-11-09 15:41:08 +0100

    React

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -1,5 +1,19 @@
 import Intro from "./modules/intro";
 import Overlay from "./modules/overlay";
+import React from "react";
+import { Router, Route, IndexRoute, browserHistory } from "react-router";
+import ReactDOM from "react-dom";
+
+import Main from "./modules/main_component";
+
+const routes = (
+	<Router history={browserHistory}>
+		<Route path="/" component={Main}></Route>
+	</Router>
+);
+
+ReactDOM.render(routes, document.querySelector(".app"));
+
 const intro = new Intro();
 const overlays = new Overlay();
 
